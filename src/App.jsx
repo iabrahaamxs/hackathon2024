5383ef54a2fb0e9f2520fb164f0224fbd12c7c8b
rosalinda <abreuparrarosalinda@gmail.com>
2024-09-25 09:36:13 -0400

    added donor reports

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home.jsx";
 import Consulta from "./pages/Consulta.jsx";
 import FAQ from "./pages/FAQ.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
+import Donante from "./pages/Donante.jsx";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/preguntas-frecuentes" element={<FAQ />} />
         <Route path="/consulta" element={<Consulta />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/donante" element={<Donante />} />
       </Routes>
     </BrowserRouter>
   );
