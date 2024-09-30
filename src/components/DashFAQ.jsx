fa88b3dcf19c3bb762ca6f524ec5bfc0f8d7cd3b
rosalinda <abreuparrarosalinda@gmail.com>
2024-09-30 12:40:13 -0400

    added logic

--- src/components/DashFAQ.jsx
+++ src/components/DashFAQ.jsx
@@ -7,26 +7,137 @@ import "../stylesheets/InputField.css";
 import { useNavigate } from "react-router-dom";
 import { LocalStorage } from "../utils/LocalStorage";
 import { FaqApi } from "../api/FaqApi";
+import Swal from 'sweetalert2';
 
 function DashFAQ() {
   const [openQuestionIndex, setOpenQuestionIndex] = useState(null);
   const [showModal, setShowModal] = useState(false);
   const [titleModal, setTitleModal] = useState("");
+  const [selectedQuestion, setSelectedQuestion] = useState(null);
+  const [questions, setQuestions] = useState([]);
+  const navigate = useNavigate();
 
   const handleQuestionClick = (index) => {
     setOpenQuestionIndex(openQuestionIndex === index ? null : index);
   };
 
-  const clickShowModal = (type) => {
-    type === "add"
-      ? setTitleModal("Agregar pregunta frecuente")
-      : setTitleModal("Actualizar pregunta frecuente");
+  const handleQuestionDelete = async (index) => {
+    const result = await Swal.fire({
+      title: '¿Estás seguro?',
+      text: "¿Quieres eliminar esta pregunta?",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, eliminar',
+      cancelButtonText: 'Cancelar'
+    });
+
+    if (result.isConfirmed) {
+      try {
+        const jwt = LocalStorage.Get("token");
+        //TODO Eliminar pregunta
+
+        Swal.fire(
+            '¡Eliminado!',
+            'La pregunta ha sido eliminada.',
+            'success'
+        );
+
+        // Recargar las preguntas
+        const faq = await FaqApi.getFaq(jwt);
+        setQuestions(faq);
+      } catch (error) {
+        console.error("Error:", error);
+        Swal.fire('Error', 'Hubo un problema al eliminar la pregunta.', 'error');
+      }
+    }
+  };
+
+  const clickShowModal = (type, question = null) => {
+    if (type === "add") {
+      setTitleModal("Agregar pregunta frecuente");
+      setSelectedQuestion(null);
+    } else {
+      setTitleModal("Actualizar pregunta frecuente");
+      setSelectedQuestion(question);
+    }
     setShowModal(true);
   };
 
-  const [questions, setQuestions] = useState([]);
+  const handleSave = async () => {
+    const result = await Swal.fire({
+      title: '¿Estás seguro?',
+      text: selectedQuestion ? "¿Quieres actualizar esta pregunta?" : "¿Quieres agregar esta pregunta?",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, guardar',
+      cancelButtonText: 'Cancelar'
+    });
 
-  const navigate = useNavigate();
+    if (result.isConfirmed) {
+      try {
+        const jwt = LocalStorage.Get("token");
+        if (selectedQuestion) {
+          //TODO Actualizar pregunta
+        } else {
+          const newQuestion = {
+            question: document.querySelector('.form').value,
+            answer: document.querySelector('.textarea').value
+          };
+          //TODO Crear nueva pregunta
+        }
+
+        Swal.fire(
+            '¡Guardado!',
+            selectedQuestion ? 'La pregunta ha sido actualizada.' : 'La pregunta ha sido agregada.',
+            'success'
+        );
+
+        setShowModal(false);
+        // Recargar las preguntas
+        const faq = await FaqApi.getFaq(jwt);
+        setQuestions(faq);
+      } catch (error) {
+        console.error("Error:", error);
+        Swal.fire('Error', 'Hubo un problema al guardar la pregunta.', 'error');
+      }
+    }
+  };
+
+  const toggleVisibility = async (id, currentView) => {
+    const result = await Swal.fire({
+      title: '¿Estás seguro?',
+      text: currentView ? "¿Quieres ocultar esta pregunta?" : "¿Quieres hacer visible esta pregunta?",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, cambiar',
+      cancelButtonText: 'Cancelar'
+    });
+
+    if (result.isConfirmed) {
+      try {
+        const jwt = LocalStorage.Get("token");
+        //TODO cambiar estado de la pregunta
+        Swal.fire(
+            '¡Actualizado!',
+            currentView ? 'La pregunta ha sido ocultada.' : 'La pregunta es ahora visible.',
+            'success'
+        );
+
+        // Recargar las preguntas
+        const faq = await FaqApi.getFaq(jwt);
+        setQuestions(faq);
+      } catch (error) {
+        console.error("Error:", error);
+        Swal.fire('Error', 'Hubo un problema al actualizar la visibilidad de la pregunta.', 'error');
+      }
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -44,65 +155,80 @@ function DashFAQ() {
     };
 
     fetchData();
-  }, []);
+  }, [navigate]);
 
   return (
-    <>
-      <div
-        style={{
-          backgroundColor: "white",
-          padding: 10,
-          display: "flex",
-          borderRadius: "10px",
-          justifyContent: "space-between",
-        }}
-      >
-        <h3>Preguntas frecuentes</h3>
-        <Button
-          variant={"primary"}
-          children={"Agregar pregunta"}
-          onClick={() => clickShowModal("add")}
-        />
-      </div>
-      <br />
-      <div
-        style={{
-          backgroundColor: "white",
-          padding: 10,
-          display: "flex",
-          flexDirection: "column",
-          borderRadius: "10px",
-        }}
-      >
-        <Modal
-          show={showModal}
-          style={{ display: "flex", padding: 30 }}
-          handleClose={() => setShowModal(false)}
+      <>
+        <div
+            style={{
+              backgroundColor: "white",
+              padding: 10,
+              display: "flex",
+              borderRadius: "10px",
+              justifyContent: "space-between",
+            }}
         >
-          <div style={{ display: "grid", gap: 15 }}>
-            <h2>{titleModal}</h2>
-            <InputField type="text" label="Pregunta" className="form" />
-            <label for="myTextarea">Respuesta</label>
-            <textarea className="textarea" rows="4" cols="80"></textarea>
-
-            <Button variant={"primary"} children={"Guardar"} />
-          </div>
-        </Modal>
-        <div className="Faq">
-          {questions.map((q, index) => (
-            <QuestionAdmin
-              key={index}
-              question={q.question}
-              answer={q.answer}
-              view={q.view}
-              isOpen={openQuestionIndex === index}
-              onClick={() => handleQuestionClick(index)}
-              btnUpdate={() => clickShowModal("update")}
-            />
-          ))}
+          <h3>Preguntas frecuentes</h3>
+          <Button
+              variant={"primary"}
+              children={"Agregar pregunta"}
+              onClick={() => clickShowModal("add")}
+          />
         </div>
-      </div>
-    </>
+        <br />
+        <div
+            style={{
+              backgroundColor: "white",
+              padding: 10,
+              display: "flex",
+              flexDirection: "column",
+              borderRadius: "10px",
+            }}
+        >
+          <Modal
+              show={showModal}
+              style={{ display: "flex", padding: 30 }}
+              handleClose={() => setShowModal(false)}
+          >
+            <div style={{ display: "grid", gap: 15 }}>
+              <h2>{titleModal}</h2>
+              <InputField
+                  type="text"
+                  label="Pregunta"
+                  className="form"
+                  value={selectedQuestion ? selectedQuestion.question : ""}
+                  onChange={(e) => setSelectedQuestion({ ...selectedQuestion, question: e.target.value })}
+              />
+              <label htmlFor="myTextarea">Respuesta</label>
+              <textarea
+                  className="textarea"
+                  rows="4"
+                  cols="80"
+                  value={selectedQuestion ? selectedQuestion.answer : ""}
+                  onChange={(e) => setSelectedQuestion({ ...selectedQuestion, answer: e.target.value })}
+              ></textarea>
+              <Button variant={"primary"} children={"Guardar"} onClick={handleSave} />
+            </div>
+          </Modal>
+          <div className="Faq">
+            {questions.map((q, index) => (
+                <QuestionAdmin
+                    key={index}
+                    id={q.id}
+                    question={q.question}
+                    answer={q.answer}
+                    view={q.view}
+                    isOpen={openQuestionIndex === index}
+                    onClick={() => handleQuestionClick(index)}
+                    btnUpdate={() => clickShowModal("update", q)}
+                    btnDelete={() => handleQuestionDelete(index)}
+                    toggleVisibility={toggleVisibility}
+                />
+            ))}
+          </div>
+        </div>
+      </>
   );
 }
+
 export default DashFAQ;
--- src/components/QuestionAdmin.jsx
+++ src/components/QuestionAdmin.jsx
@@ -3,61 +3,68 @@ import "../stylesheets/Question.css";
 import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 import { useState } from "react";
 
-function QuestionAdmin({ question, answer, isOpen, onClick, view, btnUpdate }) {
+function QuestionAdmin({ question, answer, isOpen, onClick, view, btnUpdate, btnDelete, id, toggleVisibility }) {
   const [isView, setIsView] = useState(view);
+
+  const handleToggleVisibility = () => {
+    toggleVisibility(id, isView);
+    setIsView(!isView);
+  };
+
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 10,
-      }}
-    >
       <div
-        className={`question-container ${isOpen ? "active" : ""}`}
-        style={{ position: "static" }}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 10,
+          }}
       >
-        <div className={`question ${isOpen ? "active" : ""}`} onClick={onClick}>
-          <h3 className={"question-text"}>{question}</h3>
-          {isOpen ? (
-            <FaAngleUp className="arrow" />
-          ) : (
-            <FaAngleDown className="arrow" />
+        <div
+            className={`question-container ${isOpen ? "active" : ""}`}
+            style={{ position: "static" }}
+        >
+          <div className={`question ${isOpen ? "active" : ""}`} onClick={onClick}>
+            <h3 className={"question-text"}>{question}</h3>
+            {isOpen ? (
+                <FaAngleUp className="arrow" />
+            ) : (
+                <FaAngleDown className="arrow" />
+            )}
+          </div>
+          {isOpen && (
+              <div className={"answer"}>
+                <p className={"answer-text"}>{answer}</p>
+              </div>
           )}
         </div>
-        {isOpen && (
-          <div className={"answer"}>
-            <p className={"answer-text"}>{answer}</p>
-          </div>
-        )}
+        <div className="actions-column" style={{ marginBottom: 20 }}>
+          <button
+              onClick={handleToggleVisibility}
+              className="action-btn"
+              style={{ color: "black" }}
+              aria-label="Edit"
+          >
+            {isView ? <FaEye /> : <FaEyeSlash />}
+          </button>
+          <button
+              onClick={btnUpdate}
+              className="action-btn"
+              aria-label="Edit"
+              style={{ color: "black" }}
+          >
+            <FaPencil />
+          </button>
+          <button
+              onClick={btnDelete}
+              className="action-btn"
+              aria-label="Delete"
+              style={{ color: "black" }}
+          >
+            <FaTrash />
+          </button>
+        </div>
       </div>
-      <div className="actions-column" style={{ marginBottom: 20 }}>
-        <button
-          onClick={() => setIsView(!isView)}
-          className="action-btn"
-          style={{ color: "black" }}
-          aria-label="Edit"
-        >
-          {isView ? <FaEye /> : <FaEyeSlash />}
-        </button>
-        <button
-          onClick={btnUpdate}
-          className="action-btn"
-          aria-label="Edit"
-          style={{ color: "black" }}
-        >
-          <FaPencil />
-        </button>
-        <button
-          className="action-btn"
-          aria-label="Delete"
-          style={{ color: "black" }}
-        >
-          <FaTrash />
-        </button>
-      </div>
-    </div>
   );
 }
 
